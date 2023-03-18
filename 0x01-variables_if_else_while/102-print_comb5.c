4acc6edf545b12313301cf68b32e6a7a70769698
esraaAlna3as <esraa.alna3as@gmail.com>
2023-03-17 17:50:19 -0700

    try final task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int x;
+	int z;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+	for (j = 0 ; j <= 9 ; j++)
+	{
+	for (x = j + 1 ; x <= 9 ; x++)
+	{
+	for (z = x + 1 ; z <= 9 ; z++)
+	{
+	putchar(i + '0');
+	putchar(j + '0');
+	putchar(' ');
+	putchar(x + '0');
+	putchar(z + '0');
+	if (i != 7)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
