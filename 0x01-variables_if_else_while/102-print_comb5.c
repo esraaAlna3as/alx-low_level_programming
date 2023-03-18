2e88387e6e7a66b365b7c36aa779083a31632202
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-03-18 03:29:44 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,30 +8,20 @@ int main(void)
 {
 	int i;
 	int j;
-	int x;
-	int z;
-	int v = 1;
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
 	for (j = 0 ; j <= 9 ; j++)
 	{
-	for (x = 0 ; x <= 9 ; x++)
-	{
-	for (z = v ; z <= 9 ; z++)
-	{
 	putchar(i + '0');
 	putchar(j + '0');
 	putchar(' ');
-	putchar(x + '0');
-	putchar(z + '0');
-	if (i != 7)
+	putchar(i + '0');
+	putchar(j + 1 + '0');
+	if (i != 9)
 	{
 	putchar(',');
 	putchar(' ');
-	}
-	v--;
-	}
 	}
 	}
 	}
