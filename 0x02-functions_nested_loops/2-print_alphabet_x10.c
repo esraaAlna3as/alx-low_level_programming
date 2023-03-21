fd979c32e08f2853375250ed4c968470863c004d
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-03-22 01:11:53 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,11 +3,7 @@
  * print_alphabet_x10 - print alphabet from a to z 10 times
  * takes no parameters
  *Return: void
- * _putchar - prints the character ch
- * @ch: parameter to print
- * Return: 1 or 0
  */
-int _putchar(char ch);
 
 void print_alphabet_x10(void)
 {
