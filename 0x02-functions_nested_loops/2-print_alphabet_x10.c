e5a78630280fede17a50ee1c379574548959eec2
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-03-22 00:50:37 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,12 @@ void print_alphabet_x10(void)
 	char ch;
 	int i = 0;
 
-	for (i = 0 ; i <= 10 ; i++)
+	for (i = 0 ; i <= 9 ; i++)
 	{
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
-	_putchar(ch);
-	}
-	_putchar('\n');
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+		{
+		_putchar(ch);
+		}
+		_putchar('\n');
 	}
 }
