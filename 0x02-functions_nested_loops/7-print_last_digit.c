ff0107ca0f2732e2cb53db997729f8802df9fd94
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-03-22 03:41:07 +0200

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include "6-abs.c"
 /**
- * print_last_digit : by use the modulo operator
+ *print_last_digit prints the last digit of a number
  *using _abs : to handle negative values
  * @n: integer to get last digit of
  * Return: last digit of n
