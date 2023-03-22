f63cae5abe477f9bcf26d303ff794dd37331a2ea
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-03-22 02:12:13 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -18,5 +18,12 @@ void print_alphabet(void);
  * Return: void
  */
 void print_alphabet_x10(void);
+/**
+*function that checks for lowercase character.
+*Returns 1 if @c is lowercase
+*Returns 0 otherwise
+*/
+int _islower(int c);
+
 
 #endif
