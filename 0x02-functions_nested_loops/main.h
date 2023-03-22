454f85dfcbfe987c56842a80ef260454057d93ba
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-03-22 02:39:51 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -19,11 +19,50 @@ void print_alphabet(void);
  */
 void print_alphabet_x10(void);
 /**
-*function that checks for lowercase character.
-*Returns 1 if @c is lowercase
-*Returns 0 otherwise
-*/
+ *function that checks for lowercase character.
+ *Returns 1 if @c is lowercase
+ *Returns 0 otherwise
+ */
 int _islower(int c);
-
+/**
+ *function that checks for alphabetic character.
+ *Returns 1 if @c is a letter, lowercase or uppercase
+ *Returns 0 otherwise
+ */
+int _isalpha(int c);
+/**
+*function print_sign that prints the sign of a number.
+*Returns 1 and prints + if @n is greater than zero
+*Returns 0 and prints 0 if @n is zero
+*Returns -1 and prints - if @n is less than zero
+*/
+int print_sign(int n);
+/**
+ *function that computes the absolute value of an integer.
+ */
+int _abs(int);
+/**
+ *function that prints the last digit of a number.
+ *Returns the value of the last digit
+ */
+int print_last_digit(int);
+/**
+ *function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ */
+void jack_bauer(void);
+/**
+ *function that prints the 9 times table, starting with 0.
+ */
+void times_table(void);
+/**
+ *function that adds two integers and returns the result.
+ */
+int add(int, int);
+/**
+ * function that prints all natural numbers from n to 98, followed by a new line.
+ *The first printed number the number passed to your function
+ *The last printed number should be 98
+ */
+void print_to_98(int n);
 
 #endif
