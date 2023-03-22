5890d389cffbbf872213a712ccb20099afad6177
esraaAlna3as <esraa.alna3as@gmail.com>
2023-03-21 19:20:58 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "maain.h"
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		while (i >= 98)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+			i--;
+		}
+	} else
+	{
+		while (i <= 98)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+			i++;
+		}
+	}
+	printf("\n");
+}
