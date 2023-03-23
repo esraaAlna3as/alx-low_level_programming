b997fb28b28ce64c0daa53dfdbd706665dbf38ff
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-03-23 08:19:03 +0200

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10) /**i never increases !*/
+	/**while (i < 10) 
 	{
-	putchar(i);/**i never increases !*/
-	}
+	putchar(i);
+	}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
