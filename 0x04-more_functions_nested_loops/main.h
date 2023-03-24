78e08d4c015662be320dbc31fa81d932dbc72e11
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-03-24 02:59:12 +0200

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,5 +9,22 @@ int _isupper(int c);
 
 int _isdigit(int c);
 
-#endif /* MAIN_H */
+int mul(int a, int b);
 
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+void print_number(int n);
+
+#endif /* MAIN_H */
