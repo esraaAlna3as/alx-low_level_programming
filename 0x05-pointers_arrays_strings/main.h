1c7d119cec752ebd18cf3072cb625efd89ef47d8
esraaAlna3as <esraa.alna3as@gmail.com>
2023-03-28 01:05:19 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset n to 98
+ * @n : number to change it value
+ **/
+
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
