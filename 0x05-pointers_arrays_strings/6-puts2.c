29f1fcdc0f22b8620a20df4f974f1c669475a6ac
esraaAlna3as <esraa.alna3as@gmail.com>
2023-03-28 16:59:14 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - prints a string
+ * @str : char pointer
+ *
+ * puts2: prints a string followed by a new line
+ *
+ */
+void puts2(char *str)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (b < a)
+	{
+		_putchar(str[b]);
+		b += 2;
+	}
+	_putchar('\n');
+}
