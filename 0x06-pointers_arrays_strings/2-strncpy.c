5adc6462bab02df5d00a47e8e1d20b34bbbc9c1a
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-03-30 08:13:41 +0200

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,16 +8,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (src[i] != '\0' && i < n)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	while (i < n)
+	for (; i < n; i++)
 	{
 		dest[i] = '\0';
-		i++;
 	}
 	return (dest);
 }
