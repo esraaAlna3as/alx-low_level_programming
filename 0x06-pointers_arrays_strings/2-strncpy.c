cf07cd6fecdde57de381d4fe670bcca51e6d2558
esraaAlna3as <esraa.alna3as@gmail.com>
2023-03-29 22:47:46 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncpy -copy string
+ * @dest: char pointer
+ * @src: char pointer
+ * @n: bytes
+ *
+ * Return: @dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i]
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
