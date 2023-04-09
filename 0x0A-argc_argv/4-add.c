1c211b142693af35a18c601d5f4b61c6027d4bf8
esraaAlna3as <esraa.alna3as@gmail.com>
2023-04-08 22:03:01 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define UNUSED(x) (void)(x)
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0 in succses and 1 in Error
+ */
+/**
+* _isdigit -  function that checks for a digit (0 through 9).
+* @c : character to check
+* Return: 1 if digit 0 if not
+*/
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	return (1);
+	else
+	return (0);
+}
+int main(int argc, char  *argv[])
+{
+	int i, sum = 0;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+	}
+	for (i = 1 ; i < argc ; i++)
+	{
+		if (!(_isdigit(atoi(argv[i]))))
+		{
+		printf("Error\n");
+		return (1);
+		}
+		sum = sum + atoi(argv[i]);
+	}
+		printf("%d\n", sum);
+		return (0);
+}
