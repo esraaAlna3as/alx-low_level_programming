040464abb6ba103934f3df2c219444c910b73d5f
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-04-11 03:14:03 +0200

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,23 +9,22 @@
  */
 int main(int argc, char *argv[])
 {
+	int cents = atoi(argv[1]);
+	int coins[] = {25, 10, 5, 2, 1};
+	int count = 0;
+	
 	if (argc != 2)
 	{
 	printf("Error\n");
 	return (1);
 	}
 
-	int cents = atoi(argv[1]);
-
 	if (cents <= 0)
 	{
 	printf("0\n");
 	return (0);
 	}
-
-	int coins[] = {25, 10, 5, 2, 1};
-	int count = 0;
-
+	
 	for (int i = 0; i < 5; i++)
 	{
 	while (cents >= coins[i])
