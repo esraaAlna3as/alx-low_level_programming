c4cf20c9434cbb437da24640f5e0a85005d8f4d0
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-04-12 02:11:02 +0200

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,5 +3,11 @@
 
 char *create_array(unsigned int size, char c);
 
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
 
 #endif /* MAIN_H */
