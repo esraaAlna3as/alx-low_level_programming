fac5c8171094e08ac7b0cac68bdb562c3621fa6a
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-04-20 04:42:25 +0200

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 #include "3-calc.h"
 /**
- * main - main function
- * @argc: arguments
- * @argv: array of args
- * Return: return something
+ * get_op_func - gets the right func
+ * @s: function s
+ * Return: int value
  */
-int main(int argc, char *argv[])
+int (*get_op_func(char *s))(int, int)
 {
-	if (argc == 4)
-	{
-		int a;
-		int b;
-		int (*func)(int, int);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-		a = atoi(argv[1]);
-		b = atoi(argv[3]);
-		func = get_op_func(argv[2]);
-		if ((*argv[2] == '%' || *argv[2] == '/') && b == 0)
-		{
-			printf("Error\n");
-			exit(100);
-		}
-		printf("%d\n", func(a, b));
-		return (0);
+	i = 0;
+	while (ops[i].f != NULL)
+	{
+		if (*s == *(ops[i].op) && s[1] == '\0')
+			return (ops[i].f);
+		i++;
 	}
+
 	printf("Error\n");
-	exit(98);
+	exit(99);
 }
