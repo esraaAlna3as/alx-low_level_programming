ff82daba0c949ffbd75dbe66078b6312059c294d
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-04-21 02:47:44 +0200

    bettty

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,45 +9,45 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	  va_list args;
-    int i = 0;
-    char *s;
-    char c;
+	va_list args;
+	int i = 0;
+	char *s;
+	char c;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && format[i])
-    {
-        switch (format[i])
-        {
-            case 'c':
-                c = (char)va_arg(args, int);
-                printf("%c", c);
-                break;
+	while (format && format[i])
+	{
+	switch (format[i])
+	{
+		case 'c':
+			c = (char)va_arg(args, int);
+			printf("%c", c);
+		break;
 
-            case 'i':
-                printf("%d", va_arg(args, int));
-                break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+		break;
 
-            case 'f':
-                printf("%f", (float)va_arg(args, double));
-                break;
+		case 'f':
+			printf("%f", (float)va_arg(args, double));
+		break;
 
-            case 's':
-                s = va_arg(args, char *);
-                if (s == NULL)
-                    printf("(nil)");
-                else
-                    printf("%s", s);
-                break;
+		case 's':
+			s = va_arg(args, char *);
+			if (s == NULL)
+   			printf("(nil)");
+			else
+			printf("%s", s);
+		break;
 
-            default:
-                break;
-        }
-	if (format[i + 1])
-	printf(", ");
+		default:
+		break;
+	}
+		if (format[i + 1])
+		printf(", ");
 
-	i++;
+		i++;
 	}
 
 va_end(args);
