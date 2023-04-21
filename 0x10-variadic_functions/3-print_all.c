d3429a9507c604ea9c385b36dea6112de2239319
esraaAlna3as <esraa.alna3as@gmail.com>
2023-04-20 17:39:28 -0700

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,56 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - prints a string
+ * @separator: seperator char
+ * @n: number of args
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	  va_list args;
+    int i = 0;
+    char *s;
+    char c;
+
+    va_start(args, format);
+
+    while (format && format[i])
+    {
+        switch (format[i])
+        {
+            case 'c':
+                c = (char)va_arg(args, int);
+                printf("%c", c);
+                break;
+
+            case 'i':
+                printf("%d", va_arg(args, int));
+                break;
+
+            case 'f':
+                printf("%f", (float)va_arg(args, double));
+                break;
+
+            case 's':
+                s = va_arg(args, char *);
+                if (s == NULL)
+                    printf("(nil)");
+                else
+                    printf("%s", s);
+                break;
+
+            default:
+                break;
+        }
+	if (format[i + 1])
+	printf(", ");
+
+	i++;
+	}
+
+va_end(args);
+
+printf("\n");
+}
