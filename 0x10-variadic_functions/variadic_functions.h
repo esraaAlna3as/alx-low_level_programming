356f25af6eba88878e85fbea6edfc0ed7168cee3
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-04-21 03:21:07 +0200

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,10 +6,10 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int print_c(va_list a);
-int print_i(va_list a);
-int print_f(va_list a);
-int print_s(va_list a);
+void print_c(va_list a);
+void print_i(va_list a);
+void print_f(va_list a);
+void print_s(va_list a);
 /**
  * struct op - struct for 3
  * @c: character to check
