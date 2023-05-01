9a2ca3788b92139177c9323c75f755f1a88c72d7
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-05-01 15:17:20 +0200

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,9 +24,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 	new->next = *head;
 	*head = new;
-		return (new);
+		return (*head);
 	}
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; i < idx - 1 && node != NULL; i++)
 	{
 	node = node->next;
 	if (node == NULL)
