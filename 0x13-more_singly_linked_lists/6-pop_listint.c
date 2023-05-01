86473168a1bf778122da84c88371fe12ea402841
esraaAlna3as <esraaAlna3as@github.com>
2023-05-01 01:30:03 -0700

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * pop_listint - pops first node of the list
+ * @head: pointer to a pointer to the list
+ * Return: first node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *first;
+	int n;
+
+	if (head == NULL)
+	return (0);
+
+	first = *head;
+	n = first->n;
+	free(first);
+	*head = (*head)->next;
+	return (n);
+}
