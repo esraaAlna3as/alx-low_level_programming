5e0cc83965d4ee456cc4b0bd00096938c4ad6832
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-05-01 15:40:37 +0200

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,8 +37,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	return (NULL);
 	}
 	tmp = node->next;
-	new->next = tmp->next;
-	tmp->next = new;
+	node->next = new;
+	new->next = tmp;
 
 	return (new);
 }
