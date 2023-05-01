d6fb6f4d40ea625902a1d2b42a7fce5b62421a7c
esraaAlna3as <esraaAlna3as@github.com>
2023-05-01 03:09:42 -0700

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * get_nodeint_at_index - gets node at index
+ * @head: linked list at head
+ * @index: index of node to get
+ * Return: node at index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	int i;
+	listint_t *node;
+
+	if (head == NULL)
+	return (NULL);
+
+	node = head;
+	for (i = 0; i < index; i++)
+	{
+		node = node->next;
+	if (node == NULL)
+		return (NULL);
+	}
+	return (node);
+}
