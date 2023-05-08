4b00a8069682202a1b6d6c3bd9c15853d6af3494
esraaAlna3as <esraaAlna3as@github.com>
2023-05-08 16:54:39 -0700

    betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,39 +9,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    if (filename == NULL)
-    return (0);
-    
-    FILE *file = fopen(filename, "r");
-    if (file == NULL)
-    return (0);
+	if (filename == NULL)
+	return (0);
 
-    char *buffer = malloc((letters + 1) * sizeof(char));
-        if (buffer == NULL) 
-        {
-        fclose(file);
-        return (0);
-        }
+	FILE *file = fopen(filename, "r");
+	if (file == NULL)
+	return (0);
 
-    ssize_t bytesRead = fread(buffer, sizeof(char), letters, file);
-    if (bytesRead <= 0)
-    {
-        fclose(file);
-        free(buffer);
-        return (0);
-    }
+	char *buffer = malloc((letters + 1) * sizeof(char));
+	if (buffer == NULL) 
+	{	
+	fclose(file);
+ 	return (0);
+	}
 
-    buffer[bytesRead] = '\0';
-  
-    ssize_t bytesWritten = fwrite(buffer, sizeof(char), bytesRead, stdout);
-    if (bytesWritten != bytesRead) 
-    {
-        fclose(file);
-        free(buffer);
-        return (0);
-    }
+	ssize_t bytesRead = fread(buffer, sizeof(char), letters, file);
+	if (bytesRead <= 0)
+	{
+	fclose(file);
+	free(buffer);
+	return (0);
+	}
+	buffer[bytesRead] = '\0';
 
-    fclose(file);
-    free(buffer);
-    return (bytesRead);
+	ssize_t bytesWritten = fwrite(buffer, sizeof(char), bytesRead, stdout);
+	if (bytesWritten != bytesRead) 
+	{
+	fclose(file);
+ 	free(buffer);
+	return (0);
+	}
+
+	fclose(file);
+	free(buffer);
+	return (bytesRead);
 }
