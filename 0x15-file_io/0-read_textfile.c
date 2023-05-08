7b0bce9c9e3df4445a2582f2bf78bd16fa06e4a5
esraaAlna3as <122400750+esraaAlna3as@users.noreply.github.com>
2023-05-09 01:48:18 +0200

    betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
+/**
+ * read_textfile - reads a textfile
+ * @filename: file to read
+ * @letters: amount of bytes to read
+ * Return: printed letters count
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     if (filename == NULL)
-    return 0;
+    return (0);
     
     FILE *file = fopen(filename, "r");
     if (file == NULL)
